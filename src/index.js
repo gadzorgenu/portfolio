6be159f10ccec9565f97cb25ec7d7daaaf879d50
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-07-03 23:15:35 +0000

    initail commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React  from 'react';
+import ReactDOM from 'react-dom';
+import { ChakraProvider } from "@chakra-ui/react"
+import App from './App';
+
+function RootApp ({ Component }) {
+  return (
+    <ChakraProvider>
+      <Component />
+      <App />
+    </ChakraProvider>
+  )
+}
+
+ReactDOM.render(
+    <RootApp/>,
+  document.getElementById('root')
+);
